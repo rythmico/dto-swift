a8c9b442bbe70bb1b27e0a47f402df1eec298082
davdroman <d@vidroman.dev>
2021-09-19 20:39:55 +0100

    Introduce `XCTJSONKit` package
    `XCTJSONKit` consists of a set of specialized tools that allow for easy conversion, assertion, and handling of JSON entities within test suites.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "RythmicoDTO",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+    ],
     products: [
         .library(name: "RythmicoDTO", targets: ["RythmicoDTO"]),
         .library(name: "StudentDTO", targets: ["StudentDTO"]),
@@ -15,12 +19,28 @@ let package = Package(
             .target(name: "StudentDTO"),
             .target(name: "TutorDTO"),
         ]),
+
         .target(name: "StudentDTO", dependencies: [
             .target(name: "CoreDTO"),
         ]),
+
         .target(name: "TutorDTO", dependencies: [
             .target(name: "CoreDTO"),
         ]),
+
         .target(name: "CoreDTO"),
+
+        .target(
+            name: "XCTJSONKit",
+            dependencies: [.product(name: "CustomDump", package: "swift-custom-dump")],
+            linkerSettings: [.linkedFramework("XCTest")]
+        ),
+        .testTarget(name: "XCTJSONKitTests", dependencies: [
+            .target(name: "XCTJSONKit"),
+        ]),
     ]
 )
+
+package.dependencies = [
+    .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "0.1.3"),
+]
