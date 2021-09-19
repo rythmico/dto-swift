baf44f34d4f2ee672d6abd0f46d4a51c9ed9769b
davdroman <d@vidroman.dev>
2021-09-19 23:58:49 +0100

    Make `APIError` codable implementation backward compatible with legacy API

--- Sources/CoreDTO/APIError.swift
+++ Sources/CoreDTO/APIError.swift
@@ -13,3 +13,29 @@ public struct APIError<Reason: APIErrorReason>: Codable, Equatable, LocalizedErr
 extension APIError {
     public var errorDescription: String? { description }
 }
+
+// TODO: remove after backend migration.
+// Below is all backwards-compatibility support.
+extension APIError {
+    private enum CodingKeys: String, CodingKey {
+        case description
+        case reason
+
+        case errorDescription
+        case errorType
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        try self.init(
+            description: container.decodeIfPresent(String.self, forKey: .description) ?? container.decode(String.self, forKey: .errorDescription),
+            reason: container.decodeIfPresent(Reason.self, forKey: .reason) ?? container.decodeIfPresent(Reason.self, forKey: .errorType)
+        )
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(description, forKey: .description)
+        try container.encode(reason, forKey: .reason)
+    }
+}
