d378de5056a04c1fc5d18c1d695b38265abb88c6
davdroman <d@vidroman.dev>
2021-09-20 00:03:24 +0100

    Add TODOs

--- Sources/StudentDTO/APIErrorReason.swift
+++ Sources/StudentDTO/APIErrorReason.swift
@@ -2,5 +2,5 @@ import Foundation
 
 public enum APIErrorReason: String, CoreDTO.APIErrorReason {
     case unknown = "UNKNOWN"
-    case clientOutdated = "APP_OUTDATED"
+    case clientOutdated = "APP_OUTDATED" // TODO: update to "CLIENT_OUTDATED" after backend migration.
 }
--- Sources/TutorDTO/APIErrorReason.swift
+++ Sources/TutorDTO/APIErrorReason.swift
@@ -2,6 +2,6 @@ import Foundation
 
 public enum APIErrorReason: String, CoreDTO.APIErrorReason {
     case unknown = "UNKNOWN"
-    case clientOutdated = "APP_OUTDATED"
+    case clientOutdated = "APP_OUTDATED" // TODO: update to "CLIENT_OUTDATED" after backend migration.
     case tutorNotVerified = "TUTOR_NOT_VERIFIED"
 }
