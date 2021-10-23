2bd30e221a1d0bcb7351adfa12c8d5248874001f
davdroman <d@vidroman.dev>
2021-10-23 23:13:54 +0100

    Add `SIWAResponse.userID` field

--- Sources/CoreDTO/SIWA/SIWAResponse.swift
+++ Sources/CoreDTO/SIWA/SIWAResponse.swift
@@ -1,9 +1,11 @@
 import Foundation
 
 public struct SIWAResponse: Codable {
+    public let userID: String
     public let accessToken: String
 
-    public init(accessToken: String) {
+    public init(userID: String, accessToken: String) {
+        self.userID = userID
         self.accessToken = accessToken
     }
 }
