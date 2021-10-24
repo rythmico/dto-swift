0706fbfce56cc44955e4f55b52dc14caaa8e6548
davdroman <d@vidroman.dev>
2021-10-24 02:39:11 +0100

    Add `APIErrorReason.unauthorized`

--- Sources/CoreDTO/API/APIErrorReason.swift
+++ Sources/CoreDTO/API/APIErrorReason.swift
@@ -3,9 +3,11 @@ import FoundationEncore
 public protocol APIErrorReasonProtocol: Codable, Equatable, RawRepresentableWithUnknown where RawValue == String {
     static var unknown: Self { get }
     static var clientOutdated: Self { get }
+    static var unauthorized: Self { get }
 }
 
 public enum APIErrorReason: String, APIErrorReasonProtocol, CaseIterable {
     case unknown = "UNKNOWN"
     case clientOutdated = "APP_OUTDATED" // TODO: update to "CLIENT_OUTDATED" after backend migration.
+    case unauthorized = "UNAUTHORIZED"
 }
--- Sources/StudentDTO/API/APIErrorReason.swift
+++ Sources/StudentDTO/API/APIErrorReason.swift
@@ -3,4 +3,5 @@ import Foundation
 public enum APIErrorReason: String, CoreDTO.APIErrorReasonProtocol {
     case unknown = "UNKNOWN"
     case clientOutdated = "APP_OUTDATED" // TODO: update to "CLIENT_OUTDATED" after backend migration.
+    case unauthorized = "UNAUTHORIZED"
 }
