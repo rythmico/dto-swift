198ab1a95d8204d2786c89be7f587a0d33c80c8d
davdroman <d@vidroman.dev>
2021-11-01 17:37:38 +0000

    Remove unneeded `RythmicoDTOTests` target

--- Package.swift
+++ Package.swift
@@ -20,9 +20,6 @@ let package = Package(
             .target(name: "StudentDTO"),
             .target(name: "TutorDTO"),
         ]),
-        .testTarget(name: "RythmicoDTOTests", dependencies: [
-            .target(name: "RythmicoDTO"),
-        ]),
 
         .target(name: "StudentDTO", dependencies: [
             .target(name: "CoreDTO"),
