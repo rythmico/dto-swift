f1b5d172dd19cef7afae1e2ef09b376ea8ddd3c8
davdroman <d@vidroman.dev>
2021-11-03 02:06:29 +0000

    Unit test `LessonPlanRequestSchedule`'s Codable conformance

--- Tests/StudentDTOTests/LessonPlanRequest/LessonPlanRequestScheduleTests.swift
+++ Tests/StudentDTOTests/LessonPlanRequest/LessonPlanRequestScheduleTests.swift
@@ -1,5 +1,5 @@
 import StudentDTO
-import XCTest
+import XCTJSONKit
 
 final class LessonPlanRequestScheduleTests: XCTestCase {
     func testInit() throws {
@@ -22,5 +22,14 @@ final class LessonPlanRequestScheduleTests: XCTestCase {
             Duration(minutes: 60),
             Duration(minutes: 90),
         ])
+    }
+
+    func testCodable() throws {
+        let sut = try LessonPlanRequestSchedule(
+            start: DateOnly(year: 2021, month: 07, day: 21),
+            time: TimeOnly(hour: 17, minute: 24),
+            duration: Duration(hours: 2, minutes: 10, seconds: 5)
+        )
+        try XCTAssertJSONCoding(sut)
     }
 }
