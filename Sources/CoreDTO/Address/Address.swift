c96dc41fa5c830ae81d8fafe3c65ef193eea4b88
davdroman <d@vidroman.dev>
2021-11-03 19:11:52 +0000

    Introduce `Address` DTO

---
+++ Sources/CoreDTO/Address/Address.swift
@@ -0,0 +1,46 @@
+import FoundationEncore
+
+public struct Address: AddressProtocol, Codable, Hashable {
+    public typealias ID = Tagged<Self, String>
+
+    public var id: ID
+    public var latitude: Double
+    public var longitude: Double
+    public var line1: String
+    public var line2: String
+    public var line3: String
+    public var line4: String
+    public var district: String
+    public var city: String
+    public var state: String
+    public var postcode: String
+    public var country: String
+
+    public init(
+        id: ID,
+        latitude: Double,
+        longitude: Double,
+        line1: String,
+        line2: String,
+        line3: String,
+        line4: String,
+        district: String,
+        city: String,
+        state: String,
+        postcode: String,
+        country: String
+    ) {
+        self.id = id
+        self.latitude = latitude
+        self.longitude = longitude
+        self.line1 = line1
+        self.line2 = line2
+        self.line3 = line3
+        self.line4 = line4
+        self.district = district
+        self.city = city
+        self.state = state
+        self.postcode = postcode
+        self.country = country
+    }
+}
---
+++ Tests/CoreDTOTests/Address/AddressTests.swift
@@ -0,0 +1,38 @@
+import CoreDTO
+import XCTJSONKit
+
+final class AddressTests: XCTestCase {
+    let sut = Address(
+        id: "ID",
+        latitude: 1,
+        longitude: 2,
+        line1: "A",
+        line2: "B",
+        line3: "C",
+        line4: "D",
+        district: "E",
+        city: "F",
+        state: "G",
+        postcode: "H",
+        country: "I"
+    )
+
+    func testInit() throws {
+        XCTAssertEqual(sut.id, "ID")
+        XCTAssertEqual(sut.latitude, 1)
+        XCTAssertEqual(sut.longitude, 2)
+        XCTAssertEqual(sut.line1, "A")
+        XCTAssertEqual(sut.line2, "B")
+        XCTAssertEqual(sut.line3, "C")
+        XCTAssertEqual(sut.line4, "D")
+        XCTAssertEqual(sut.district, "E")
+        XCTAssertEqual(sut.city, "F")
+        XCTAssertEqual(sut.state, "G")
+        XCTAssertEqual(sut.postcode, "H")
+        XCTAssertEqual(sut.country, "I")
+    }
+
+    func testJSONCoding() throws {
+        try XCTAssertJSONCoding(sut)
+    }
+}
