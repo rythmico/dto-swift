b7fc8ce7a8b7d118463e2dac837c179914c3984e
davdroman <d@vidroman.dev>
2021-11-19 19:08:41 +0000

    Rename BOOKING_REQUESTS_CHANGED to LESSON_PLAN_REQUESTS_CHANGED

--- Sources/TutorDTO/API/APIEvent.swift
+++ Sources/TutorDTO/API/APIEvent.swift
@@ -3,6 +3,6 @@ import FoundationEncore
 public typealias APIEvent = Knowable<KnownAPIEvent>
 
 public enum KnownAPIEvent: String, APIEventProtocol {
-    case bookingRequestsChanged = "BOOKING_REQUESTS_CHANGED"
+    case lessonPlanRequestsChanged = "LESSON_PLAN_REQUESTS_CHANGED"
     case bookingApplicationsChanged = "BOOKING_APPLICATIONS_CHANGED"
 }
